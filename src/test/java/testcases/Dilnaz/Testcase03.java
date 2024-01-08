f6daa0f139a80d7c7e1c5e7dd0bbab34d0324634
SarojChokrungjaroentaworn <c-saroj@hotmail.com>
2024-01-08 18:07:11 -0500

    up

---
+++ src/test/java/testcases/Dilnaz/Testcase03.java
@@ -0,0 +1,22 @@
+package testcases.Dilnaz;
+
+import org.testng.annotations.Test;
+
+import utilities.CommonMethods;
+
+public class Testcase03 extends CommonMethods {
+
+	@Test
+	public void adRedirection() throws InterruptedException {
+		getDriver();
+//		click(hp.womenBotton);
+//		click(hp.topsSubMenu);
+//
+//		Thread.sleep(3000);
+//		Select s = new Select(hp.sortBy);
+//
+//		s.selectByValue("name");
+//	
+
+	}
+}
